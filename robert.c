21f1aff9a15cc64a67eb0642c5676c9ae47567b3
Robert Avramescu <robert_valentin196@yahoo.ro>
2018-03-01 21:28:53 +0200

    added robert header for problem solution

---
+++ robert.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "robert.h"
+
+int verificare_masina_valabila(int x);
+{
+    printf("merge");
+    return 0;
+}
---
+++ robert.h
@@ -0,0 +1,6 @@
+#ifndef ROBERT_H
+#define ROBERT_H
+int verificare_masina_valabila(int x);
+
+
+#endif // ROBERT_H_INCLUDED
