7871585363fe0d4c181e21680ab776a19674e01c
Viorel Alexandrescu <alex.viorel.117@gmail.com>
2018-03-01 22:01:55 +0200

    Fixed read.h again

--- read.h
+++ read.h
@@ -1,37 +1,42 @@
 #include "entities.h"
 #include <stdio.h>
+
 unsigned int numberOfRides;
+FILE* input;
 
-FILE* input = fopen(filePath, "r");
+void initInputFile(const char* filePath)
+{
+    input = fopen(filePath, "r");
+}
 
-info readDetails(const char *filePath;)
+info readDetails(const char *filePath)
 {
     info problemDetails;
-    fscanf(input, "%u%u%u%u%u%u", &problemDetails.numberOfRows, &problemDetails.numberOfColumns, &problemDetails.numberOfCars, &problemDetails.numberOfRides, &problemDetails.bonus, &problemDetails.numberOfSteps)
+    fscanf(input, "%u%u%u%u%u%u", &problemDetails.numberOfRows, &problemDetails.numberOfColumns, &problemDetails.numberOfCars, &problemDetails.numberOfRides, &problemDetails.bonus, &problemDetails.numberOfSteps);
     numberOfRides = problemDetails.numberOfRides;
     return problemDetails;
 }
 
-ride readRides(const char *filePath;)
+ride* readRides(const char *filePath)
 {
     ride curse[numberOfRides];
 
     int i;
-    for(i=0;i<numberOfRides;i++)
+    for(i = 0; i < numberOfRides; i++)
     {
         fscanf(input, "%u %u %u %u %u %u" , &curse[i].startX, &curse[i].startY, &curse[i].finishX, &curse[i].finishY, &curse[i].earliestStart, &curse[i].latestFinish);
     }
     return curse;
 }
 
-void printRides(const char *filePath, car* cars)
+void printRides(const char *filePath, car* cars, unsigned int numberOfCars)
 {
     FILE* output = fopen(filePath, "w");
-    int i,j;
-    for(i=0;i<problemDetalis.numberOfCars,i++)
+    int i, j;
+    for(i = 0; i < numberOfCars; i++)
     {
         fprintf(output, "%u ", cars[i].id);
-        for(j=0;j<cars[i].ridesDone,j++)
+        for(j = 0; j < cars[i].ridesDone; j++)
         {
             fprintf(output, "%u ",cars[i].rides[j].id);
         }
