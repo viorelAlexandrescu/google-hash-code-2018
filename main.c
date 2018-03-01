9a26f96ad4b465af3912efc4de29d6e6eded0b63
Robert Avramescu <robert_valentin196@yahoo.ro>
2018-03-01 21:32:28 +0200

    changed file structure and added codeblocks dependency file

--- main.c
+++ main.c
@@ -1,9 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "entities.h"
+#include "robert.h"
 
 int main()
 {
-    printf("Hello world!\n");
+    unsigned int curent_step=0; // nr actual de step
+//    while(curent_step<=)
+//    {
+        verificare_masina_valabila(2);
+//    }
+//
     return 0;
 }
--- robert.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include "robert.h"
-
-int verificare_masina_valabila(int x);
-{
-    printf("merge");
-    return 0;
-}
